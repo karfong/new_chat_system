48e13c0aa49a79b149a33e713b7e97a6404a745d
Fong <chankarfong8888@gmail.com>
2026-05-25 21:42:42 +0800

    inital commit

---
+++ backend/src/routes/auth.route.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get("/signup", (req, res) => {
+    res.json({ message: "Signup endpoint" });
+})
+
+router.get("/login", (req, res) => {
+    res.json({ message: "Login endpoint" });
+})
+
+router.get("/logout", (req, res) => {
+    res.json({ message: "Logout endpoint" });
+})
+
+export default router;

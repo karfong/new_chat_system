4ffc74737803eed81b0ade8cdd09f9ccb5621688
Fong <chankarfong8888@gmail.com>
2026-06-17 19:41:05 +0800

    implemented welcome email

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,16 +1,17 @@
 import jwt from "jsonwebtoken";
+import { ENV } from "./env.js";
 
 export const generateToken = (userId, res) => {
-    const { JWT_SECRET } = process.env;
+    const { JWT_SECRET } = ENV;
     if (!JWT_SECRET) throw new Error("JWT_SECRET is not defined");
 
-    const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "7d" });
+    const token = jwt.sign({ userId }, ENV.JWT_SECRET, { expiresIn: "7d" });
 
     res.cookie("jwt", token, {
         maxAge: 7 * 24 * 60 * 60 * 1000, //MS
         httpOnly: true, //prevent XSS attacks
         sameSite: "strict", //CSRF protection
-        secure: process.env.NODE_ENV === "production" ? true : false,
+        secure: ENV.NODE_ENV === "production" ? true : false,
     });
 
     return token;
